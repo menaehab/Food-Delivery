531732d579d76f54e64c299e68eb4fb6ad9dd256
menaehab <commanderyt123@gmail.com>
2024-10-23 00:16:49 +0300

    Setup common css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "../../styles/Header.css";
+export default function Home() {
+  return <div>Home</div>;
+}
