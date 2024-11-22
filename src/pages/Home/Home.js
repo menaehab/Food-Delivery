ac6f3a099e22bdda068e361213763869330cea19
Mena Ehab <commanderyt123@gmail.com>
2024-11-22 23:49:42 +0200

    Setup shop section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import Hero from "../Home/Hero";
 import Explore from "./Explore";
 import Menu from "./Menu";
 import Promotion from "./Promotion";
+import Shop from "./Shop";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Explore />
         <Menu />
         <Promotion />
+        <Shop />
       </Layout>
     </>
   );
