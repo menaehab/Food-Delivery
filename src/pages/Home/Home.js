b3a8b9f5bd5ac7f336f063dc2833e12605ba8de2
Mena Ehab <commanderyt123@gmail.com>
2024-11-22 20:53:11 +0200

    Setup promotion section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Layout from "../../assets/components/layouts/Layout";
 import Hero from "../Home/Hero";
 import Explore from "./Explore";
 import Menu from "./Menu";
+import Promotion from "./Promotion";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Hero />
         <Explore />
         <Menu />
+        <Promotion />
       </Layout>
     </>
   );
